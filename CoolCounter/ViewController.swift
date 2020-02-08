9f3d8c2931f30b6eba83410381b8e3a872bcca18
Ilya Biltuev <ilyabiltuev@gmail.com>
2020-02-08 19:16:01 +0300

    Initial Commit

---
+++ CoolCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoolCounter
+//
+//  Created by biltuevs on 08/02/2020.
+//  Copyright © 2020 ilyabiltuev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
