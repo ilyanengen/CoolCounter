ac3b6d8e697c46849d58e87f02ec97d0367db927
Ilya Biltuev <ilyabiltuev@gmail.com>
2020-02-08 20:16:09 +0300

    basic data source + dark mode

--- CoolCounter/ViewController.swift
+++ CoolCounter/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterPickerView: UIPickerView!
+    
+    private var count: Int = 0
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickButtonTouched() {
+        
+        count += 1
+        
+        counterPickerView.reloadAllComponents()
+    }
 }
 
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 4
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return 10
+    }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return String(row)
+    }
+    
+}
